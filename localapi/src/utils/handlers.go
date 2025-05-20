37da0e81c6e65289e5f044402018fca00039351a
IyNew <wtang3@nd.edu>
2025-05-20 17:26:42 +0000

    more documentation

--- localapi/src/utils/handlers.go
+++ localapi/src/utils/handlers.go
@@ -15,21 +15,7 @@ type CertificateDBObject struct {
 	SerializedCertificate string `json:"serialized_certificate"`
 }
 
-func GetCertificateDBObject(cert *Certificate) (*CertificateDBObject, error) {
-	serializedCert, err := json.Marshal(cert)
-	if err != nil {
-		return nil, err
-	}
-	cert_db_obj := &CertificateDBObject{
-		PilotID:               cert.PilotID,
-		CertificateID:         GetRandomString(10),
-		DroneID:               cert.DroneID,
-		ExpirationDate:        cert.ExpirationDate,
-		SerializedCertificate: string(serializedCert),
-	}
-	return cert_db_obj, nil
-}
-
+// 
 // get a random base64 string
 func GetRandomString(n int) string {
 	b := make([]byte, n)
